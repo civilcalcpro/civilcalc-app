02fea885d27bc474b25bbd923a6625e0272edeb2
Civilcalc <civilcal592005@gmail.com>
2026-05-25 18:26:30 +0530

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://civilcalc-app.vercel.app',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://civilcalc-app.vercel.app/login',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://civilcalc-app.vercel.app/signup',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://civilcalc-app.vercel.app/pricing',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://civilcalc-app.vercel.app/forgot-password',
+      lastModified: new Date(),
+    },
+  ]
+}
