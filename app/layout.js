d773eb0ddb88d7dcd742f4842353bae0e29a953e
Civilcalc <civilcal592005@gmail.com>
2026-05-31 09:43:43 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -77,10 +77,11 @@ export default function RootLayout({ children }) {
             position="top-right"
           />
 
-          <Analytics />
-          <SpeedInsights />
-        </AuthProvider>
-              <Script
+         <Analytics />
+<SpeedInsights />
+
+{/* Google Analytics */}
+<Script
   src="https://www.googletagmanager.com/gtag/js?id=G-WY9TW5Y54J"
   strategy="afterInteractive"
 />
@@ -90,8 +91,18 @@ export default function RootLayout({ children }) {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
     gtag('config', 'G-WY9TW5Y54J');
+  `}
+</Script>
+
+{/* Microsoft Clarity */}
+<Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wzivn1hsg7");
   `}
 </Script>
       </body>
