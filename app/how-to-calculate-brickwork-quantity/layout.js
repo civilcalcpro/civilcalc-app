6293715c6837bfe6b68908149501996f7e3ccb21
Civilcalc <civilcal592005@gmail.com>
2026-06-24 01:25:51 +0530

    Create layout.js

---
+++ app/how-to-calculate-brickwork-quantity/layout.js
@@ -0,0 +1,23 @@
+export const metadata = {
+  title: 'How to Calculate Brickwork Quantity | CivilCalc Pro',
+  description:
+    'Learn how to calculate brickwork quantity, number of bricks, mortar volume, cement and sand requirement for brick masonry.',
+  alternates: {
+    canonical: 'https://civilcalcpro.in/how-to-calculate-brickwork-quantity',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-snippet': -1,
+      'max-image-preview': 'large',
+      'max-video-preview': -1,
+    },
+  },
+}
+
+export default function Layout({ children }) {
+  return children
+}
