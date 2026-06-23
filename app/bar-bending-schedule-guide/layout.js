bc3d22840ca7ad9ff9f07ab7fb86fc357c6f413c
Civilcalc <civilcal592005@gmail.com>
2026-06-24 00:59:19 +0530

    Create layout.js

---
+++ app/bar-bending-schedule-guide/layout.js
@@ -0,0 +1,23 @@
+export const metadata = {
+  title: 'Bar Bending Schedule Guide | BBS for Civil Engineers',
+  description:
+    'Learn bar bending schedule basics, BBS calculation method, reinforcement cutting length and steel quantity estimation.',
+  alternates: {
+    canonical: 'https://civilcalcpro.in/bar-bending-schedule-guide',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-snippet': -1,
+      'max-image-preview': 'large',
+      'max-video-preview': -1,
+    },
+  },
+}
+
+export default function Layout({ children }) {
+  return children
+}
