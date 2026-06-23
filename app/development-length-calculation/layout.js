26c34c2cccfae1c72d05ec4955733a904c752868
Civilcalc <civilcal592005@gmail.com>
2026-06-24 00:59:42 +0530

    Create layout.js

---
+++ app/development-length-calculation/layout.js
@@ -0,0 +1,23 @@
+export const metadata = {
+  title: 'Development Length Calculation | CivilCalc Pro',
+  description:
+    'Understand development length calculation for reinforcement bars with formula, examples and civil engineering explanation.',
+  alternates: {
+    canonical: 'https://civilcalcpro.in/development-length-calculation',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-snippet': -1,
+      'max-image-preview': 'large',
+      'max-video-preview': -1,
+    },
+  },
+}
+
+export default function Layout({ children }) {
+  return children
+}
